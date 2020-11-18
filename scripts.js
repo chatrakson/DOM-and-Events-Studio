32215dc17f6b845567036bff6faf4f4b6925db4d
chatrakson <chat.rakson@gmail.com>
2020-11-18 00:20:16 -0600

    up and down working left and right does not

--- scripts.js
+++ scripts.js
@@ -6,10 +6,10 @@ let btnRight = null;
 let btnLeft = null;
 let btnTakeOff = null;
 let btnLand = null;
+let btnAbort = null;
 
 window.addEventListener("load", function () {
     btnTakeOff = document.getElementById("takeoff");
-
     btnTakeOff.addEventListener("click", function(event) {
         if (window.confirm("Confirm that the shuttle is ready for takeoff.")) {
             document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
@@ -19,11 +19,47 @@ window.addEventListener("load", function () {
     });
 
     btnLand = document.getElementById("landing");
-
     btnLand.addEventListener("click", function(event) {
         window.alert("The shuttle is landing. Landing gear engaged.");
         document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
         document.getElementById("shuttleBackground").style.backgroundColor = "green";
         document.getElementById("spaceShuttleHeight").innerHTML = "0";
     });
+
+    btnAbort = document.getElementById("missionAbort");
+    btnAbort.addEventListener("click", function(event) {
+        if (window.confirm("Confirm that you want to abort the mission.")) {
+            document.getElementById("flightStatus").innerHTML = "Mission aborted.";
+            document.getElementById("shuttleBackground").style.backgroundColor = "green";
+            document.getElementById("spaceShuttleHeight").innerHTML = "0";
+        };
+    });
+
+    let newYPosition = document.getElementById("rocket").offsetTop;
+    let newXPosition = document.getElementById("rocket").offsetLeft;
+
+    btnUp = document.getElementById("up");
+    btnUp.addEventListener("click", function(event) {
+        newYPosition = newYPosition - 10;
+        document.getElementById("rocket").style.top = newYPosition + "px";
+    });
+
+    btnDown = document.getElementById("down");
+    btnDown.addEventListener("click", function(event) {
+        newYPosition = newYPosition + 10;
+        document.getElementById("rocket").style.top = newYPosition + "px";
+    });
+
+    btnLeft = document.getElementById("left");
+    btnLeft.addEventListener("click", function(event) {
+        newXPosition = newXPosition - 10;
+        document.getElementById("rocket").style.left = newXPosition + "px";
+    });
+
+    btnRight = document.getElementById("right");
+    btnRight.addEventListener("click", function(event) {
+        newXPosition = newXPosition + 10;
+        document.getElementById("rocket").style.right = newXPosition + "px";
+    });
+
 });
