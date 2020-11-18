62937aa3307e4e5281b87342c2ca7fc048590c78
chatrakson <chat.rakson@gmail.com>
2020-11-17 22:18:57 -0600

    Added 'Landing Button' events

--- scripts.js
+++ scripts.js
@@ -12,9 +12,18 @@ window.addEventListener("load", function () {
 
     btnTakeOff.addEventListener("click", function(event) {
         if (window.confirm("Confirm that the shuttle is ready for takeoff.")) {
-            document.getElementById("flightStatus").innerHTML = "Shuttle in flight."
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight.";
             document.getElementById("shuttleBackground").style.backgroundColor = "blue";
             document.getElementById("spaceShuttleHeight").innerHTML = "10,000";
         };
+    });
+
+    btnLand = document.getElementById("landing");
+
+    btnLand.addEventListener("click", function(event) {
+        window.alert("The shuttle is landing. Landing gear engaged.");
+        document.getElementById("flightStatus").innerHTML = "The shuttle has landed.";
+        document.getElementById("shuttleBackground").style.backgroundColor = "green";
+        document.getElementById("spaceShuttleHeight").innerHTML = "0";
     });
 });
