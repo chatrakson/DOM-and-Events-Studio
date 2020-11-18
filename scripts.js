36af14041da9a3b45c8d887055ed48e9ad427ed7
chatrakson <chat.rakson@gmail.com>
2020-11-17 21:24:07 -0600

    Added 'take off' button events

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let btnDown = null;
+let btnUp = null;
+let btnRight = null;
+let btnLeft = null;
+let btnTakeOff = null;
+let btnLand = null;
+
+window.addEventListener("load", function () {
+    btnTakeOff = document.getElementById("takeoff");
+
+    btnTakeOff.addEventListener("click", function(event) {
+        if (window.confirm("Confirm that the shuttle is ready for takeoff.")) {
+            document.getElementById("flightStatus").innerHTML = "Shuttle in flight."
+            document.getElementById("shuttleBackground").style.backgroundColor = "blue";
+            document.getElementById("spaceShuttleHeight").innerHTML = "10,000";
+        };
+    });
+});
